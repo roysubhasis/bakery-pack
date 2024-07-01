09f7dd6ec98c79ef52fda670d73a4488740820ff
roysubhasis <subhasis.it@gmail.com>
2024-07-02 01:21:05 +0530

    Solution and Test case code added

--- src/main/java/com/bakery/pack/BakerySolution.java
+++ src/main/java/com/bakery/pack/BakerySolution.java
@@ -1,4 +1,31 @@
 package com.bakery.pack;
 
 public class BakerySolution {
+    private static final int[] packageArr = { 9, 5, 3 };  // no of packages
+
+    private static String finalOutput = null;
+    private static final int totalInput = 34; // number of orders
+
+    public static void main(String[] args) {
+        System.out.println(calculateOutput(totalInput));
+    }
+
+    public static String calculateOutput(int totalInputOrder) {
+        find(totalInputOrder, 0, "");
+        return finalOutput.substring(1);
+    }
+
+    private static void find(int totalInputOrder, int i, String str) {
+        if (totalInputOrder == 0) {
+            finalOutput = str;
+        }
+        if (totalInputOrder < 0) {
+            return;
+        }
+
+        for (int j = i; j < packageArr.length; j++) {
+            if (finalOutput == null)
+                find(totalInputOrder - packageArr[j], j, str + "," + packageArr[j]);
+        }
+    }
 }
